a6d458af182edcabffec1bfe210249748e9c1dc3
Yudai NAKATA <yudai.nkt@gmail.com>
2018-09-30 17:17:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# vivaldi-mod
+
+A collection of my custom CSS for [Vivaldi][vivaldi].
+
+## Overview
+This repository contains Vivaldi customizations of mine and a deployment script written in Bash.
+Note that the script is only for macOS at the moment.
+
+## License
+This repository is released under the MIT License.
+See [LICENSE.md](./LICENSE.md) for details.
+
+[vivaldi]: https://vivaldi.com/
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $(uname) != "Darwin" ]]; then
+  echo "This script is intended to be used in macOS, thus aborting."
+  exit 1
+fi
+
+if ! [[ -d /Applications/Vivaldi.app/ ]]; then
+  echo "You need to install Vivaldi to utilize this script."
+  exit 1
+fi
+
+version=$(mdls -name kMDItemVersion /Applications/Vivaldi.app | awk -F '"' '{print $2}')
+dir="/Applications/Vivaldi.app/Contents/Versions/${version}/Vivaldi Framework.framework/Resources/vivaldi"
+
+for file in *.css; do
+  is_deployed=$(grep -c "<link rel=\"stylesheet\" href=\"style/${file}\" />" "${dir}/browser.html")
+
+  if [[ ${is_deployed} -ge 2 ]]; then
+    echo "Something went wrong; ${file} is read more then once. Check out browser.html."
+    exit 1
+  fi
+
+  cp "${file}" "${dir}/style"
+
+  if [[ ${is_deployed} -eq 0 ]]; then
+    perl -pi -e "s/<\/head>/  <link rel=\"stylesheet\" href=\"style\/${file}\" \/>\n  <\/head>/" "$dir/browser.html"
+  fi
+
+  echo "${file} is successfully deployed."
+done
